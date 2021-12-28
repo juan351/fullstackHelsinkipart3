dea510014bfa528a05f6a10e8d6b28769bdf8469
juan351 <montiverolp@yahoo.com>
2021-12-28 00:10:18 -0300

    3.22 phonebook lint

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const Person = require('./models/person')
 const express = require('express')
-const req = require('express/lib/request')
 const app = express()
 let morgan = require('morgan')
 const cors = require('cors')
@@ -13,90 +12,90 @@ morgan.token('body', (req,res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })    
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
-    Person.find({}).then(persons => {
-        response.send(`<p>Phonebook has info for ${persons.length} people </p><p>${new Date().toString()}</p>`)
-    })
-    
+  Person.find({}).then(persons => {
+    response.send(`<p>Phonebook has info for ${persons.length} people </p><p>${new Date().toString()}</p>`)
+  })
+
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person => {
-        if(person) {
-            response.json(person)
-        }else{
-            response.status(404).end()
-        }
-    })
-    .catch(error => next(error)) 
-       
+  Person.findById(request.params.id).then(person => {
+    if(person) {
+      response.json(person)
+    }else{
+      response.status(404).end()
+    }
+  })
+    .catch(error => next(error))
+
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-        response.status(204).end()
+app.delete('/api/persons/:id', (request, response,next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
     })
     .catch(error => next(error))
-    
+
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    
-    if(!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-    if(!body.number) {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
-    
+  const body = request.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number        
+  if(!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
     })
-    
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
+  }
+  if(!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
     })
+  }
+
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
     .catch(error => next(error))
-    
+
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedPerson => {
-        response.json(updatedPerson)
+      response.json(updatedPerson)
     })
     .catch(error => next(error))
-    
+
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-    if (error.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidatorError'){
-        return response.status(400).send( {error: error.message})
-    }
-    next(error)
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidatorError'){
+    return response.status(400).send( { error: error.message })
+  }
+  next(error)
 }
 
 app.use(errorHandler)
--- mongo.js
+++ mongo.js
@@ -4,39 +4,39 @@ if (process.argv.length < 3) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
   process.exit(1)
 }else {
-    const password = process.argv[2]
+  const password = process.argv[2]
 
-    const url = ``
+  const url = ''
 
-    mongoose.connect(url)
+  mongoose.connect(url)
 
-    const personSchema = new mongoose.Schema({
-        name: String,
-        number: String,
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+  })
+
+  const Person = mongoose.model('Person', personSchema)
+
+  if(process.argv.length > 3){
+    const person = new Person({
+      name: process.argv[3],
+      number: process.argv[4],
+    })
+
+    person.save().then(result => {
+      console.log('added', person.name, 'number', person.number, 'to phonebook' )
+      mongoose.connection.close()
+    })
+
+  }else{
+    Person.find({}).then(result => {
+      result.forEach(person => {
+        console.log(person)
       })
-    
-    const Person = mongoose.model('Person', personSchema)
+      mongoose.connection.close()
+    })
 
-    if(process.argv.length > 3){
-        const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4],
-        })
-
-        person.save().then(result => {
-        console.log('added', person.name, 'number', person.number, 'to phonebook' )
-        mongoose.connection.close()
-        })
-
-    }else{
-        Person.find({}).then(result => {
-            result.forEach(person => {
-              console.log(person)
-            })
-            mongoose.connection.close()
-          })
-
-    }
+  }
 }
 
 
